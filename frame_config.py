016f3a56d6b0f0a077e9ae7803fa1f6d6e6fd437
Dmitry Utkin <dmitryure@gmail.com>
2026-04-28 23:04:14 +0300

    missed file

--- frame_config.py
+++ frame_config.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from typing import Any, Mapping, Sequence
-
+from collections.abc import Mapping, Sequence
+from typing import Any
 
 FRAME_CONFIG_KEY = "frames"
 
